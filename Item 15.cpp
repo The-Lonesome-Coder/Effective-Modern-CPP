bddb832963f087b02b83c0d5ac63ec7683482377
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-12-17 01:26:40 +0800

    Use constexpr whenever possible

--- Item 15.cpp
+++ Item 15.cpp
@@ -1,6 +1,16 @@
 #include <array>
 /**
  * Use constexpr whenever possible.
+ *
+ *
+ * constexpr objects are const and are initialized with values known during compilation.
+ *
+ * constexpr functions can produce compile-time results when called with arguments whose values are known during
+ * compilation.
+ *
+ * constexpr objects and functions may be used in a wider range of contexts than non-constexpr objects and functions.
+ *
+ * constexpr is part of an object’s or function’s interface.
 */
 
 /**
