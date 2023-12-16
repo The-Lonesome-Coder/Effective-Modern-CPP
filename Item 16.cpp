e4a47041aa51c77bd2f9eb522482b0d86215a6b6
The-Lonesome-Coder <the.lonesome.coder@gmail.com>
2023-12-17 01:26:51 +0800

    Make const member functions thread safe

--- Item 16.cpp
+++ Item 16.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 /**
  * Make const member functions thread safe.
+ *
+ *
+ * Make const member functions thread safe unless you’re certain they’ll never be used in a concurrent context.
+ *
+ * Use of std::atomic variables may offer better performance than a mutex, but they’re suited for manipulation of only a
+ * single variable or memory location.
 */
 
 /**
